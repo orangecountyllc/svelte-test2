5a39e641f7402e2f9682b014f89b50c2ccea48b7
Jay <jpca999@gmail.com>
2025-06-18 20:06:33 -0400

    -

--- svelte.config.js
+++ svelte.config.js
@@ -6,10 +6,13 @@ const config = {
 		adapter: adapter({
       strict: false  // This ignores missing pages during build
     }),
+	 prerender: {
+      handleHttpError: 'warn', // Prevents crash on 404s during prerender
+    },
+
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
 	}
 };
 
